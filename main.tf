df2593acc2ed5c3bcb56489e3743a5fb79fd27f4
Emilija13 <89068860+Emilija13@users.noreply.github.com>
2025-07-08 11:11:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-test-emilija"
-    region         = "eu-north-1"
+    region         = "us-east-1"
     key            = "s3-github-actions/terraform.tfstate"
     encrypt = true
   }
