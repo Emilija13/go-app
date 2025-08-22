999664d8f783391bdbfd1f9a0b2d30f761f9d973
Emilija13 <emilijalakinska13@gmail.com>
2025-08-22 16:05:58 +0200

    adding PodDisruptionBudget and podAntiAffinity

--- main.tf
+++ main.tf
@@ -171,7 +171,7 @@ resource "aws_eks_node_group" "default" {
   node_group_name = "default"
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = aws_subnet.public[*].id
-  instance_types  = ["t3.micro"]
+  instance_types  = ["t3.small"]
 
   scaling_config {
     desired_size = 2
